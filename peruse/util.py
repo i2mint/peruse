c79dd0d1922be10b573b35711a2d8bac8f963780
Thor Whalen <thorwhalen1@gmail.com>
2021-01-14 17:37:16 -0800

    correlation_detection

--- peruse/util.py
+++ peruse/util.py
@@ -1,6 +1,15 @@
+from functools import partial
+import numpy as np
 from numpy import ceil, zeros, hanning, fft
+import matplotlib.pyplot as plt
 
 DFLT_WIN_FUNC = hanning
+
+
+def named_partial(name, func, *args, **kwargs):
+    partial_func = partial(*args, **kwargs)
+    partial_func.name = name
+    return partial_func
 
 
 def stft(y, n_fft=2048, hop_length=None, win_func=DFLT_WIN_FUNC):
@@ -32,3 +41,23 @@ def stft(y, n_fft=2048, hop_length=None, win_func=DFLT_WIN_FUNC):
     S = S[:n_fft // 2 + 1, :]
 
     return S
+
+
+def pad_to_align(*arrays):
+    """Yields arrays that are padded (on the right) with zeros so as to all be of the same length
+
+    >>> x, y, z = pad_to_align([1, 2], [1, 2, 3, 4], [1, 2, 3])
+    >>> x, y, z
+    (array([1, 2, 0, 0]), array([1, 2, 3, 4]), array([1, 2, 3, 0]))
+
+    """
+    max_len = max(map(len, arrays))
+    for arr in arrays:
+        yield np.pad(arr, (0, max_len - len(arr)))
+
+
+def pplot(*args, figsize=(22, 5), **kwargs):
+    """The same old matplotlib plot, but with some custom defaults"""
+    if figsize is not None:
+        plt.figure(figsize=figsize)
+    return plt.plot(*args, **kwargs)
